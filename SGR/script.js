21b0ae9405a2d92d295ea3fca19dd398b98077a9
mk-LR <33muneishi3@gmail.com>
2025-04-16 17:14:01 +0900

    Update script.js

--- SGR/script.js
+++ SGR/script.js
@@ -5,11 +5,18 @@ let deck = [];
 let scorePlayer = 0;
 let scoreCp1 = 0;
 let scoreCp2 = 0;
+let handPlayer = 0;
+let handCp1 = 0;
+let handCp2 = 0;
 let lastSuit = "JOKER";
 let lastRank = "None";
 let count = 0;
 let turn = 0;
 let isPlayerTurn = true;
+let isJackPlayer = false;
+let isJackCpu1 = false;
+let isJackCpu2 = false;
+let isQueen = false;
 let centerZ = 0;
 let logtext = "sample"
 
@@ -58,19 +65,24 @@ function renderDeck() {
     cardEl.onclick = () => {
       if (!isPlayerTurn || cardEl.classList.contains("face-up-F") || cardEl.classList.contains("face-down-H1") || cardEl.classList.contains("face-down-H2")) return;
 
-      if (turn < 3){
+      if (turn < 3 || isQueen){
         cardEl.classList.remove("face-down");
         cardEl.classList.add("face-up-H");
         cardEl.textContent = card.label;
-        cardEl.style.left = `${turn*100+170}px`;
+        cardEl.style.left = `${handPlayer*100+170}px`;
         cardEl.style.top = `500px`;
-        computerTurn(1);
-        computerTurn(2);
-        turn++;
-        if (turn === 3) {
-          document.getElementById("turn-indicator").textContent = "あなたのターン";
-          document.getElementById("card-count").textContent = `出すカードを選ぼう`
-        }
+        handPlayer++;
+        isQueen = false;
+        setTimeout(() => {
+          computerTurn(1);
+          if (turn < 3) computerTurn(2);
+          else isPlayerTurn = false;
+          turn++;
+          if (turn === 3) {
+            document.getElementById("turn-indicator").textContent = "あなたのターン";
+            document.getElementById("card-count").textContent = `出すカードを選ぼう`;
+          }
+        }, 500); 
       }
       else revealCard(cardEl, card);
       if (card.suit === "♥") cardEl.style.color = "red";
@@ -81,6 +93,58 @@ function renderDeck() {
     };
     container.appendChild(cardEl);
   });
+}
+
+function nextTurn(logtext) {
+  if (logtext != "skip"){
+    if (lastRank === "None") {
+      document.getElementById("card-count").textContent = `積み重なっているカード：${count}枚　　ログ：${logtext} → None`
+      setTimeout(centerPlace, 1000);
+    }
+    else if (lastRank === "JOKER") {
+      document.getElementById("card-count").textContent = `積み重なっているカード：${count}枚　　ログ：${logtext} → JOKER`
+    }
+    else {
+      document.getElementById("card-count").textContent = `積み重なっているカード：${count}枚　　ログ：${logtext} → ${lastSuit}${lastRank}`
+    }
+  }
+  turn++;
+  if (turn % 3 === 0) {
+    document.getElementById("turn-indicator").textContent = "あなたのターン";
+    if (isQueen) document.getElementById("turn-indicator").textContent = "Qドロー：手札選択";
+    isPlayerTurn = true;
+  }
+  else {
+    document.getElementById("turn-indicator").textContent = `CPU${turn % 3}のターン`;
+    if (isQueen) document.getElementById("turn-indicator").textContent = "Qドロー：手札選択中...";
+    isPlayerTurn = false;
+    setTimeout(() => {
+      computerTurn(turn % 3);
+    }, 2500);
+  }
+  const allCards = document.querySelectorAll(".card");
+  const available = Array.from(allCards).filter(c => c.classList.contains("face-down"));
+  if (available.length === 0) {
+    const effectEl = document.getElementById("suit-effect");
+    if (scorePlayer <= scoreCp1 && scorePlayer <= scoreCp2) effectEl.textContent = "WIN";
+    else effectEl.textContent = "LOSE";
+    effectEl.classList.add("show");
+    effectEl.classList.remove("hidden");
+  }
+}
+
+function showEffect(text, logtext) {
+  const effectEl = document.getElementById("suit-effect");
+  effectEl.textContent = text;
+  effectEl.classList.add("show");
+  effectEl.classList.remove("hidden");
+  setTimeout(() => {
+    effectEl.classList.remove("show");
+    setTimeout(() => {
+      effectEl.classList.add("hidden");
+      nextTurn(logtext);
+    }, 500);
+  }, 1000);
 }
 
 function revealCard(cardEl, card) {
@@ -105,23 +169,57 @@ function revealCard(cardEl, card) {
   if (lastRank === "None") logtext = "None";
   else if (lastRank === "JOKER") logtext = "JOKER";
   else logtext = `${lastSuit}${lastRank}`;
+  if (isJackPlayer && (turn % 3 === 0)) {
+    isJackPlayer = false;
+    const jackB = document.querySelectorAll(".j-skip");
+    jackB[0].remove();
+  }
   if (lastSuit === card.suit || lastRank === card.rank || lastSuit === "JOKER" || card.suit === "JOKER") {
     if (count > 1) {
       if (card.rank === "A") {
         turn--;
+        showEffect("ワンモアA", logtext);
       }
-      if (card.rank === "J") {
+      else if (card.rank === "J") {
+        if (turn % 3 === 0) {
+          isJackPlayer = true;
+          jackPlace();
+        }
+        else if (turn % 3 === 1) isJackCpu1 = true;
+        else isJackCpu2 = true;
+        showEffect("Jスキップ", logtext);
+      }
+      else if (card.rank === "Q") {
         turn--;
+        isQueen = true;
+        showEffect("Qドロー", logtext);
       }
-      if (card.rank === "Q") {
+      else if (card.rank === "K") {
+        if (turn % 3 === 0) {
+          scorePlayer -= count;
+          document.getElementById("scorePlayer").textContent = scorePlayer;
+        }
+        else if (turn % 3 === 1) {
+          scoreCp1 -= count;
+          document.getElementById("scoreCp1").textContent = scoreCp1;
+        }
+        else {
+          scoreCp2 -= count;
+          document.getElementById("scoreCp2").textContent = scoreCp2;
+        }
         turn--;
+        count = 0;
+        showEffect("Kルール", logtext);
       }
-      if (card.rank === "K") {
-        turn--;
-      }
+      else showEffect("Safe", logtext);
     }
+    else showEffect("Safe", logtext);
     lastSuit = card.suit;
     lastRank = card.rank;
+    if (count === 0) {
+      lastSuit = "JOKER"
+      lastRank = "None"
+    }
   }
   else {
     if (turn % 3 === 0) {
@@ -139,32 +237,9 @@ function revealCard(cardEl, card) {
     lastSuit = "JOKER"
     lastRank = "None"
     count = 0;
+    nextTurn(logtext);
   }
-
   cardEl.onclick = null;
-  // 次のターン
-  if (lastRank === "None") {
-    document.getElementById("card-count").textContent = `積み重なっているカード：${count}枚　　ログ：${logtext} → None`
-    setTimeout(centerPlace, 1000);
-  }
-  else if (lastRank === "JOKER") {
-    document.getElementById("card-count").textContent = `積み重なっているカード：${count}枚　　ログ：${logtext} → JOKER`
-  }
-  else {
-    document.getElementById("card-count").textContent = `積み重なっているカード：${count}枚　　ログ：${logtext} → ${lastSuit}${lastRank}`
-  }
-  turn++;
-  if (turn % 3 === 0) {
-    document.getElementById("turn-indicator").textContent = "あなたのターン";
-    isPlayerTurn = true;
-  }
-  else {
-    document.getElementById("turn-indicator").textContent = `CPU${turn % 3}のターン`;
-    isPlayerTurn = false;
-    setTimeout(() => {
-      computerTurn(turn % 3);
-    }, 2000);
-  }
 }
 
 function centerPlace() {
@@ -179,9 +254,32 @@ function centerPlace() {
   container.appendChild(placeEl);
 }
 
+function jackPlace() {
+  const container = document.getElementById("card-ring");
+  const jackEl = document.createElement("div");
+  jackEl.id = "j-skip";
+  jackEl.className = "j-skip";
+  jackEl.textContent = "スキップ"
+  jackEl.style.left = `10px`;
+  jackEl.style.top = `510px`;
+  jackEl.style.zIndex = 0;
+  jackEl.onclick = () => {
+    if (!isPlayerTurn) return;
+    jackEl.remove();
+    turn++;
+    document.getElementById("turn-indicator").textContent = `CPU${turn % 3}のターン`;
+    isPlayerTurn = false;
+    isJackPlayer = false;
+    setTimeout(() => {
+      computerTurn(turn % 3);
+    }, 2000);
+  }
+  container.appendChild(jackEl);
+}
+
 function computerTurn(cpu) {
   const allCards = document.querySelectorAll(".card");
-  if (turn < 3) {
+  if (turn < 3 || isQueen) {
     const x = 650;
     let y = 150;
     if (cpu === 2) y += 150;
@@ -190,30 +288,71 @@ function computerTurn(cpu) {
     const choice = available[Math.floor(Math.random() * available.length)];
     choice.classList.remove("face-down");
     choice.classList.add(`face-down-H${cpu}`);
-    choice.style.left = `${turn*20+x}px`;
+    if (cpu === 1) {
+      choice.style.left = `${handCp1*20+x}px`;
+      handCp1++;
+    }
+    else {
+      choice.style.left = `${handCp2*20+x}px`;
+      handCp2++;
+    }
     choice.style.top = `${y}px`;
     choice.style.zIndex = turn;
-  }
-  else {
-    let exist = true;
-    const allhands = Array.from(allCards).filter(c => c.classList.contains(`face-down-H${cpu}`));
-    if (allhands.length > 0) {
-      for (let cardEl of allhands) {
-        const index = parseInt(cardEl.dataset.index);
-        const card = deck[index];
-        if (lastSuit === card.suit || lastRank === card.rank || lastSuit === "JOKER" || card.suit === "JOKER") {
-          revealCard(cardEl, card);
-          return;
-        }
+    if (isQueen) {
+      isQueen = false;
+      turn++;
+      if (turn % 3 === 0) {
+        document.getElementById("turn-indicator").textContent = "あなたのターン";
+        isPlayerTurn = true;
+      }
+      else {
+        document.getElementById("turn-indicator").textContent = `CPU${turn % 3}のターン`;
+        isPlayerTurn = false;
+        setTimeout(() => {
+          computerTurn(turn % 3);
+        }, 2000);
       }
     }
-    if (exist) {
-      const available = Array.from(allCards).filter(c => c.classList.contains("face-down"));
-      if (available.length === 0) return;
-      const choice = available[Math.floor(Math.random() * available.length)];
-      const index = parseInt(choice.dataset.index);
-      const card = deck[index];
-      revealCard(choice, card);
+  }
+  else {
+    let skip = false;
+    if (cpu === 1) {
+      if (isJackCpu1) {
+        isJackCpu1 = false;
+        if (count > 0) skip = true;
+      }
+    }
+    else {
+      if (isJackCpu2) {
+        isJackCpu2 = false;
+        if (count > 0) skip = true;
+      }
+    }
+    if (skip) {
+      skip = false;
+      showEffect(`CPU${cpu}：スキップ`, "skip");
+    }
+    else {
+      let exist = true;
+      const allhands = Array.from(allCards).filter(c => c.classList.contains(`face-down-H${cpu}`));
+      if (allhands.length > 0 && count > 0) {
+        for (let cardEl of allhands) {
+          const index = parseInt(cardEl.dataset.index);
+          const card = deck[index];
+          if (lastSuit === card.suit || lastRank === card.rank || lastSuit === "JOKER" || card.suit === "JOKER") {
+            revealCard(cardEl, card);
+            return;
+          }
+        }
+      }
+      if (exist) {
+        const available = Array.from(allCards).filter(c => c.classList.contains("face-down"));
+        if (available.length === 0) return;
+        const choice = available[Math.floor(Math.random() * available.length)];
+        const index = parseInt(choice.dataset.index);
+        const card = deck[index];
+        revealCard(choice, card);
+      }
     }
   }
 }
